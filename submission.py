9b3ebf851382343af9cfb4320ca3d1a74417a44f
TheRealColinInns <colin.inns@me.com>
2023-11-08 14:56:50 -0800

    finished

--- submission.py
+++ submission.py
@@ -6,6 +6,8 @@ if __name__=="__main__":
     states, values = model.generate(10)
     print(states)
     print(values)
+    test_results = []
+    observed = []
     with open("ambiguous_sents.obs") as observations:
         for observation in observations:
             if observation != '\n':
@@ -13,8 +15,17 @@ if __name__=="__main__":
     with open("ambiguous_sents.obs") as observations:
         for observation in observations:
             if observation != '\n':
+                observed.append(observation.strip())
                 result = " ".join(model.viterbi(observation.split()))
+                test_results.append(result)
                 print("Observation: " + observation.strip())
                 print("Result: " + result)
+    with open("ambiguous_sents.tagged.obs") as actual:
+        count = 0
+        for tagged in actual:
+            if count % 2 == 0:
+                if test_results[count//2] != tagged.strip():
+                    print("Error: " + str(observed[count//2]))
+            count += 1
 
 
